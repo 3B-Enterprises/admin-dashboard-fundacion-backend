0585609ebcf7e02e4af0767d77d914b2faddcbd7
jhadechni <jaimeluissierra88@gmail.com>
2022-12-13 23:20:05 -0500

    changes

--- src/controllers/users/getAllUsers.js
+++ src/controllers/users/getAllUsers.js
@@ -6,11 +6,6 @@ const getAllUsers = async (req, res) => {
     try {
         const users = await userModel.find({}, '-__v -_id')
 
-        for (let index = 0; index < users.length; index++) {
-            const info = users[index]
-            const QR = await QRCode.toDataURL(`${process.env.LINK_TO_FRONTEND}/${info.consecutive}`)
-            await userModel.findOneAndUpdate({ consecutive: info.consecutive }, { QR : QR })
-        }
 
         return res.status(200).json(messageBuilder(200, "Usuarios", users))
     } catch (error) {
